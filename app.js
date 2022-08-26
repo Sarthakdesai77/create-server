a01bf9a96f247cdb6ddb76364cb87b66fe5924cc
Sarthakdesai77 <princesarthakdesai@gmail.com>
2022-08-26 11:13:11 +0530

    server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http')
 
 const server = http.createServer(function (req,res){
     res.writeHead(200, {'Content-Type':'text/html'})
-    res.end('<h1>Hello from server</h1>')
+    res.end('<h1>List of movies</h1></br><h4>Avengers</h4></br><h4>Game of Thrones</h4></br><h4>House of Dragon</h4></br><h4>Thor</h4>')
 })
 
 server.listen(3000,function(){
